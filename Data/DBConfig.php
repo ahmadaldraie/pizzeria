46e27b25dd8bec2d1fad8001c6b5d421136afb10
Ahmad Al-Draie <ahmad023853382@gmail.com>
2024-09-25 17:21:00 +0200

    .

---
+++ Data/DBConfig.php
@@ -0,0 +1,11 @@
+<?php
+    declare(strict_types=1);
+
+    namespace Data;
+    
+    class DBConfig {
+        public static $DB_CONNSTRING = "";
+        public static $DB_USERNAME   = "";
+        public static $DB_PASSWORD   = "";
+    }
+?>
